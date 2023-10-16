a13d6b9aaa74cdc8d1260b0ccee738448bdd1e67
mahtab.mahdavi <mahtaabmahdavi@gmail.com>
2023-10-16 17:32:15 +0330

    add student repository

---
+++ student-course/src/main/java/com/mahtab/reactive/repository/StudentRepository.java
@@ -0,0 +1,8 @@
+package com.mahtab.reactive.repository;
+
+import com.mahtab.reactive.model.Student;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
+public interface StudentRepository extends ReactiveCrudRepository<Student, Long> {
+    boolean existsByFirstNameAndLastName();
+}
